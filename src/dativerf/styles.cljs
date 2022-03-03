b50102c3be5abd01d87a1ea0c56df6d9f234ec4d
Joel Dunham <jrwdunham@gmail.com>
2022-03-02 21:50:48 -0800

    Add GentiumPlus font family and CSS

--- src/dativerf/styles.cljs
+++ src/dativerf/styles.cljs
@@ -3,6 +3,7 @@
     [garden.def :refer [defcssfn]])
   (:require
     [spade.core   :refer [defglobal defclass]]
+    [garden.stylesheet :refer [at-font-face]]
     #_[garden.units :refer [deg px]]
     #_[garden.color :refer [rgba]]
     #_[garden.core :as garden]))
@@ -13,10 +14,23 @@
 (declare widget)
 
 (defglobal defaults
+  (at-font-face {:font-family "GentiumPlus"}
+                {:src "url(./vendor/fonts/GentiumPlus-R.woff)"})
+  (at-font-face {:font-family "GentiumPlus"
+                 :font-style "italic"}
+                {:src "url(./vendor/fonts/GentiumPlus-I.woff)"})
   [:body {:color :black}])
 
 (defclass level1 []
   {:color :black})
+
+(defclass attr-label []
+  {:width "180px"})
+
+(defclass objlang []
+  {:font-family "'GentiumPlus', serif"
+   ;; :line-height "0.5em"
+   :font-size "16pt"})
 
 (defclass widget []
   {:color :red})
