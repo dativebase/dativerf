1b50ba6ad4530e772b2f699511ee810106b2a1c7
Joel Dunham <jrwdunham@gmail.com>
2022-03-02 21:51:02 -0800

    Basic Form IGT view in its own ns

--- src/dativerf/views/form.cljs
+++ src/dativerf/views/form.cljs
@@ -4,9 +4,11 @@
             [dativerf.events :as events]
             [dativerf.routes :as routes]
             [dativerf.specs.form :as form-specs]
+            [dativerf.styles :as styles]
             [dativerf.subs :as subs]
             [dativerf.utils :as utils]
-            [dativerf.views.widgets :as widgets]))
+            [dativerf.views.widgets :as widgets]
+            [reagent.core :as reagent]))
 
 ;; Buttons
 
@@ -61,13 +63,73 @@
    {:key :morpheme-gloss :type :string}
    {:key :translations :type :coll-of-translations}])
 
+(def attrs
+  {:transcription
+   {:label "transcription"
+    :tooltip "A transcription, probably orthographic."}
+   :morpheme-break
+   {:label "morpheme break"
+    :tooltip
+    (str "A sequence of morpheme shapes and delimiters. The OLD assumes"
+         " phonemic shapes (e.g., “in-perfect”), but phonetic (i.e.,"
+         " allomorphic, e.g., “im-perfect”) ones are ok.")}
+   :morpheme-gloss
+   {:label "morpheme gloss"
+    :tooltip
+    (str "A sequence of morpheme glosses and delimiters, isomorphic to"
+         " the morpheme break sequence, e.g., “NEG-parfait”.")}
+   :translations
+   {:label "translations"}})
+
+(defn igt-label [label]
+  [re-com/box
+   :class (styles/attr-label)
+   :justify :end
+   :padding "0 1em 0 0"
+   :child label])
+
+(defn igt-transcription
+  [{:keys [attr transcription grammaticality left-enclose right-enclose]
+    :or {grammaticality "" left-enclose "" right-enclose ""}}]
+  [re-com/h-box
+   :src (at)
+   :children
+   [[igt-label (-> attr attrs :label)]
+    [re-com/box
+     :max-width "500px"
+     :child
+     (if (or (seq grammaticality) (seq transcription))
+       (str left-enclose grammaticality transcription right-enclose)
+       "")]]])
+
+(defn igt-translations [form-id translations]
+  [re-com/h-box
+   :src (at)
+   :children
+   [[igt-label (-> attrs :translations :label)]
+    [re-com/v-box
+     :max-width "500px"
+     :children
+     (for [{:keys [id grammaticality transcription]} translations]
+       ^{:key (str form-id "-" id)}
+       [re-com/box :child (str grammaticality transcription)])]]])
+
 (defn igt-form [form-id]
-  (let [form (form-specs/parse-form
-              @(re-frame/subscribe [::subs/form-by-id form-id]))]
+  (let [{:keys [grammaticality morpheme-break morpheme-gloss transcription
+                translations]}
+        (form-specs/parse-form @(re-frame/subscribe [::subs/form-by-id
+                                                     form-id]))]
     [re-com/v-box
      :src (at)
+     :class (styles/objlang)
      :children
-     (for [{:as row :keys [key]} form-rows]
-       ^{:key key} [widgets/key-value-row
-                    (utils/kebab->space (name key))
-                    (assoc row :value (key form))])]))
+     [[igt-transcription {:attr :transcription
+                          :transcription transcription
+                          :grammaticality grammaticality}]
+      [igt-transcription {:attr :morpheme-break
+                          :transcription morpheme-break
+                          :left-enclose "/"
+                          :right-enclose "/"}]
+      [igt-transcription {:attr :morpheme-gloss
+                          :transcription morpheme-gloss}]
+      [igt-translations form-id translations]]]))
