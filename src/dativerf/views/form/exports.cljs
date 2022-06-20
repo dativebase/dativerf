144a1e440db22fc4572f357a3657c517f3cc8e63
Joel Dunham <jrwdunham@gmail.com>
2022-06-20 12:56:09 -0700

    Add Leipzig IGT exporter
    - It is able to export a form into an XML string format that should
      conform to the format expected by the Liepzig IGT Javascript
      implementation at https://github.com/bdchauvette/leipzig.js.

--- src/dativerf/views/form/exports.cljs
+++ src/dativerf/views/form/exports.cljs
@@ -43,6 +43,57 @@
 (defn json-export [form]
   (-> form prepare-form-for-jsonification utils/->pretty-json))
 
+;; Leipzig IGT export
+;;
+;; The canonical reference for this XML version of the Leipzig IGT format seems
+;; to be the JavaScript implementation whose source is available at
+;; https://github.com/bdchauvette/leipzig.js/. The canonical linguistic
+;; reference for Leipzig IGT appears to be
+;; https://www.eva.mpg.de/lingua/resources/glossing-rules.php.
+;; Example::
+;;
+;;   <div data-gloss>
+;;     <p>n parqu a hanze</p>
+;;     <p>1PS.SUBJ park FV outside</p>
+;;     <p>'I park outside.'</p>
+;;   </div>
+
+(defn p-el [igt-line] (str "  <p>" igt-line "</p>"))
+
+(defn enclose-in-quotes [string] (str \' string \'))
+
+(defn p-els-for-non-nil-val
+  ([key form] (p-els-for-non-nil-val key identity form))
+  ([key formatter form]
+   [(some-> form key str/trim not-empty formatter p-el)]))
+
+(defn p-els-for-grammaticalized-transcription
+  ([form] (p-els-for-grammaticalized-transcription identity form))
+  ([formatter {:as tmp :keys [transcription grammaticality]}]
+   (p-els-for-non-nil-val :v formatter {:v (str grammaticality transcription)})))
+
+(defn p-els-for-translations [{:keys [translations]}]
+  (mapcat (partial p-els-for-grammaticalized-transcription enclose-in-quotes)
+          translations))
+
+(def leipzig-igt-rules
+  [(partial p-els-for-non-nil-val :narrow-phonetic-transcription)
+   (partial p-els-for-non-nil-val :phonetic-transcription)
+   p-els-for-grammaticalized-transcription
+   (partial p-els-for-non-nil-val :morpheme-break)
+   (partial p-els-for-non-nil-val :morpheme-gloss)
+   p-els-for-translations])
+
+(defn leipzig-igt-export [form]
+  (str "<div data-gloss>\n"
+       (or (some->> leipzig-igt-rules
+                    (mapcat (fn [rule] (rule form)))
+                    (filter some?)
+                    not-empty
+                    (str/join "\n"))
+           "  <p>no data</p>")
+       "\n</div>"))
+
 ;; API
 
 (def exports
@@ -51,7 +102,10 @@
     :efn plain-text-export}
    {:id :json
     :label "JSON"
-    :efn json-export}])
+    :efn json-export}
+   {:id :leipzig-igt
+    :label "Leipzig IGT"
+    :efn leipzig-igt-export}])
 
 (defn export [export-id]
   (first (for [{:as e :keys [id]} exports
